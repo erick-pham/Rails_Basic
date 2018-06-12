582ca413539752f0d3574aa46e1a524224fea181
Phạm Quôc Toàn <bossdiemmaimai@gmail.com>
2018-06-11 22:18:04 -0700

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.default_url_options = { host: 'https://rails-toanpham.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
-
+	config.assets.compile = true
     config.action_mailer.smtp_settings = {
         :address => 'smtp.gmail.com',
         :port => 587,
